8f18c35b72831b3e0104cbe1db67a38872657e6d
Pranav-pradeep0 <pranavpradeep1120@gmail.com>
2024-02-17 10:30:56 +0530

    path addition

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port} & ${dirname}`);
+  console.log(`Server is running on port ${port} & ${dirname} & ${path}`);
 });
